683cdffc1a6b5ccd8f1aa5e69cc17a16e374a969
Logan Born <lcborn4@gmail.com>
2024-08-04 19:08:37 -0400

    navigation route

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,17 +2,18 @@
 import { Router } from "express";
 
 import AuthRouter from './api/auth.js'
-
 import PingRouter from './api/ping.js'
-
 import UserRouter from './api/user.js'
+import NavigationRouter from './api/navigation.js'
 
 import AuthnMiddleware from '../middlewares/authn.js'
 
 const router = new Router();
 
 //auth
-router.use('/auth', AuthRouter)
+router
+  .use('/auth', AuthRouter)
+  .use('/navigation', NavigationRouter)
 
   .use(AuthnMiddleware.authenticateToken)
 
